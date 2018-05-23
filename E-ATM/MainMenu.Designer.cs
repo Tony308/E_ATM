7d274260b5ef0eb2f149bdba1ba85bbca9fa7782
Tony Huang <tonyhuang@hotmail.co.uk>
2018-05-23 21:12:16 +0100

    Project Initialization.

---
+++ E-ATM/MainMenu.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace E_ATM
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cardCB = new System.Windows.Forms.ComboBox();
+            this.enterCardLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cardCB
+            // 
+            this.cardCB.FormattingEnabled = true;
+            this.cardCB.Items.AddRange(new object[] {
+            "Credit Card",
+            "Debit Card"});
+            this.cardCB.Location = new System.Drawing.Point(40, 47);
+            this.cardCB.Name = "cardCB";
+            this.cardCB.Size = new System.Drawing.Size(121, 24);
+            this.cardCB.TabIndex = 0;
+            // 
+            // enterCardLbl
+            // 
+            this.enterCardLbl.AutoSize = true;
+            this.enterCardLbl.Location = new System.Drawing.Point(37, 27);
+            this.enterCardLbl.Name = "enterCardLbl";
+            this.enterCardLbl.Size = new System.Drawing.Size(81, 17);
+            this.enterCardLbl.TabIndex = 1;
+            this.enterCardLbl.Text = "Insert Card:";
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 250);
+            this.Controls.Add(this.enterCardLbl);
+            this.Controls.Add(this.cardCB);
+            this.Name = "MainMenu";
+            this.Text = "BotW ATM";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainMenu_FormClosed);
+            this.Load += new System.EventHandler(this.MainMenu_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.ComboBox cardCB;
+        private System.Windows.Forms.Label enterCardLbl;
+    }
+}
+
---
+++ E-ATM/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace E_ATM
+{
+    public partial class MainMenu : Form
+    {
+        private Encryption hash;
+
+        public object ConfigurationManager { get; private set; }
+
+        public MainMenu()
+        {
+            
+            InitializeComponent();
+
+        }
+        private void MainMenu_Load(object sender, EventArgs e)
+        {
+            hash = new Encryption();
+            hash.GenKey_SaveinContainer("PIN1");
+            hash.GetKeyFromContainer("PIN1");
+            hash.encrypt();
+        }
+
+        private void MainMenu_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
